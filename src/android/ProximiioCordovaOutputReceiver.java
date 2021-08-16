7477dcc4d948af0ae5e025b1d49cc29cb3be5491
markusfluxguide <75258337+markusfluxguide@users.noreply.github.com>
2021-08-16 16:03:15 +0200

    Update ProximiioCordovaOutputReceiver.java
    updated androidx import

--- src/android/ProximiioCordovaOutputReceiver.java
+++ src/android/ProximiioCordovaOutputReceiver.java
@@ -7,7 +7,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.support.v4.app.NotificationCompat;
+import androidx.core.app.NotificationCompat; 
+
 import android.util.Log;
 
 import org.json.JSONException;
